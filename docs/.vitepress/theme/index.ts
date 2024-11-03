b9ddbc5de50999adee1e8d4061b1213a6ec1763e
weizwz <1124725517@qq.com>
2024-11-03 17:31:07 +0800

    设置主题颜色动态变换

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,17 +1,39 @@
-// https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
+//.vitepress/theme/index.ts
+import { watch } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import './style.css'
+import './style/index.scss'
+import Layout from './layout/index.vue'
+import { EnhanceAppContext } from 'vitepress'
+let homePageStyle: HTMLStyleElement | undefined
 
 export default {
-  extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
+    extends: DefaultTheme,
+    Layout: Layout,
+    enhanceApp({ router }: EnhanceAppContext) {
+        if (typeof window === 'undefined') return
+        watch(
+            () => router.route.data.relativePath,
+            () => updateHomePageStyle(location.pathname === '/'),
+            { immediate: true }
+        )
+    }
 } satisfies Theme
+
+// 设置过渡动画
+function updateHomePageStyle(value: boolean) {
+    if (value) {
+        if (homePageStyle) return
+        homePageStyle = document.createElement('style')
+        homePageStyle.innerHTML = `
+        :root {
+            animation: rainbow 12s linear infinite;
+        }`
+        document.body.appendChild(homePageStyle)
+    } else {
+        if (!homePageStyle) return
+        homePageStyle.remove()
+        homePageStyle = undefined
+    }
+}
+// 如上29-31行所示，设置主题色过渡时间为12s
