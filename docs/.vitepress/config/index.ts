8d14a871a26a2f38d8fdbae09b936215fcfede91
weizwz <1124725517@qq.com>
2024-11-06 15:52:56 +0800

    配置修改，增加 vite-plugin-markdown-preview 插件

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -3,9 +3,14 @@ import { nav } from './nav'
 import { algolia } from './algolia'
 import { head } from './head'
 import { sidebar } from './sidebar'
+import MarkdownPreview from 'vite-plugin-markdown-preview'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  // 部署路径
+  base: '/',
+  outDir: '../dist',
+
   lang: 'zh-CN',
   title: 'weizwz',
   // 完全自定义标题，:title 不要动，改后面的
@@ -13,30 +18,54 @@ export default defineConfig({
   description: '个人博客 · 时光笔墨',
   // fav图标
   head,
-  // 部署路径
-  base: '/',
+
+  lastUpdated: true,
+  cleanUrls: true,
+
+  // markdown配置
+  markdown: {
+    // 行号显示
+    lineNumbers: true, // false关闭
+    image: {
+      lazyLoading: true,
+    },
+  },
+
   themeConfig: {
     // 左上角logo
     logo: '/favicon.ico',
     // 标题隐藏
     siteTitle: 'weizwz',
+    i18nRouting: false,
+
     // https://vitepress.dev/reference/default-theme-config
     nav,
 
     sidebar,
+
     // 社交按钮
     socialLinks: [{ icon: 'github', link: 'https://github.com/weizwz/blog' }],
 
-    // 侧边栏文字更改(移动端)
-    sidebarMenuLabel: '目录',
 
-    // 返回顶部文字修改(移动端)
-    returnToTopLabel: '返回顶部',
+    // 相关文字和提示修改
+    returnToTopLabel: '回到顶部', // 返回顶部文字修改(移动端)
+    sidebarMenuLabel: '菜单', // 侧边菜单
+    darkModeSwitchLabel: '主题',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
 
     // 大纲显示2-3级标题
     outline: {
       level: [2, 3],
       label: '页面导航'
+    },
+
+    lastUpdated: {
+      text: '最后更新于',
+      formatOptions: {
+        dateStyle: 'short',
+        timeStyle: 'medium',
+      },
     },
 
     // 自定义上下页名
@@ -55,9 +84,7 @@ export default defineConfig({
   sitemap: {
     hostname: 'https://weizwz.com'
   },
-  // markdown配置
-  markdown: {
-    // 行号显示
-    lineNumbers: true // false关闭
-  }
+  vite: {
+    plugins: [MarkdownPreview()],
+  },
 })
