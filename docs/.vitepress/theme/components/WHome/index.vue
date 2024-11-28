133c1b8cff37d63fa443e5df5b937e17cece4463
weizwz <1124725517@qq.com>
2024-11-29 00:33:38 +0800

    首页以及资源更新

---
+++ docs/.vitepress/theme/components/WHome/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="about">
+    <div class="container">
+      <el-row class="container-row">
+        <el-col :span="16" class="container-left">
+          <h1 class="about-title">
+            <span>唯知笔记</span>
+          </h1>
+          <h1 class="about-title">一个知识分享网站</h1>
+          <h2 class="about-desc">闲来无事会，笔墨皆尽染</h2>
+        </el-col>
+        <el-col :span="8" class="container-right">
+          <div class="hero-avatar">
+            <img src="../../../../head_bg.png" alt="" />
+            <div class="avatar-icon">
+              <ul class="list-inline">
+                <li>
+                  <a href="https://github.com/weizwz" target="_blank"><i class="github"></i></a>
+                </li>
+                <li>
+                  <a href="https://gitee.com/weizwz" target="_blank"><i class="gitee"></i></a>
+                </li>
+                <li>
+                  <a href="https://www.cnblogs.com/weizwz" target="_blank"><i class="cnblogs"></i></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,44 +1,20 @@
-import { watch } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './style/index.scss'
 import WLayout from './components/WLayout.vue'
+import WHome from './components/WHome/index.vue'
 import { EnhanceAppContext } from 'vitepress'
 import TwoslashFloatingVue from '@shikijs/vitepress-twoslash/client'
 import '@shikijs/vitepress-twoslash/style.css'
-
-let homePageStyle: HTMLStyleElement | undefined
 
 export default {
   extends: DefaultTheme,
   Layout: WLayout,
   enhanceApp({ app, router }: EnhanceAppContext) {
+    app.component("weiz-home", WHome);
     app.use(TwoslashFloatingVue)
     app.use(ElementPlus)
-    if (typeof window === 'undefined') return
-    watch(
-      () => router.route.data.relativePath,
-      () => updateHomePageStyle(location.pathname === '/'),
-      { immediate: true }
-    )
   }
 } satisfies Theme
-
-// 设置过渡动画
-function updateHomePageStyle(value: boolean) {
-  if (value) {
-    if (homePageStyle) return
-    homePageStyle = document.createElement('style')
-    homePageStyle.innerHTML = `
-        :root {
-            animation: rainbow 12s linear infinite;
-        }`
-    document.body.appendChild(homePageStyle)
-  } else {
-    if (!homePageStyle) return
-    homePageStyle.remove()
-    homePageStyle = undefined
-  }
-}
