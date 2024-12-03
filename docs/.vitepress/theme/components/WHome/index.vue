4a2b5cbd6304209ba3219998f02d4249e0cb1651
weizwz <1124725517@qq.com>
2024-12-04 01:19:36 +0800

    首页样式优化

--- docs/.vitepress/theme/components/WHome/index.vue
+++ docs/.vitepress/theme/components/WHome/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="about">
     <div class="container">
-      <el-row class="container-row">
-        <el-col :span="16" class="container-left">
+      <el-row class="container-row" :gutter="20" >
+        <el-col :xs="24" :sm="16" :md="18" class="container-left">
           <div class="title">
             <h1 class="about-title">
               <span>唯知笔记</span>
@@ -12,44 +12,36 @@
             </h1>
           </div>
           <h2 class="about-tagline">知之为知之，不知为不知</h2>
-          <div class="avatar-icon">
+          <div class="about-nav">
             <ul class="list-inline">
-              <li>
+              <li v-for="(item, index) of navList" :key="index">
                 <a href="" target="_blank">
-                  <i class="web"></i>
-                  <span>前端笔记</span>
-                </a>
-              </li>
-              <li>
-                <a href="" target="_blank">
-                  <i class="code"></i>
-                  <span>编码提效</span>
-                </a>
-              </li>
-              <li>
-                <a href="" target="_blank">
-                  <i class="blog"></i>
-                  <span>博客文档</span>
-                </a>
-              </li>
-              <li>
-                <a href="" target="_blank">
-                  <i class="resource"></i>
-                  <span>资源分享</span>
-                </a>
-              </li>
-              <li>
-                <a href="" target="_blank">
-                  <i class="other"></i>
-                  <span>其他杂项</span>
+                  <i :class="item.className"></i>
+                  <span>{{ item.title }}</span>
                 </a>
               </li>
             </ul>
           </div>
         </el-col>
-        <el-col :span="8" class="container-right">
+        <el-col :xs="24" :sm="8" :md="6" class="container-right">
           <div class="hero-avatar">
-            <img src="../../../../public/head.png" alt="" />
+            <img :src="withBase('head.png')" alt="head-avatar" />
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+  <div id="post">
+    <div class="container">
+      <el-row class="container-row" :gutter="20">
+        <el-col v-for="i of 3" :key="i" :xs="24" :sm="8" :md="6">
+          <div class="post-card">
+            1
+          </div>
+        </el-col>
+        <el-col  :xs="24" :md="6">
+          <div class="post-card">
+            2
           </div>
         </el-col>
       </el-row>
@@ -57,13 +49,17 @@
   </div>
 </template>
 <script setup lang="ts">
-import { onMounted } from 'vue'
-import { useData } from 'vitepress'
-const { title, theme, page, frontmatter } = useData()
+import { onMounted, ref } from 'vue'
+import { useData, withBase } from 'vitepress'
 
-onMounted(()=> {
-  window.document.title = title.value
-})
+const navList = ref([
+  { title: '前端笔记', className: 'web' },
+  { title: '编码提效', className: 'code' },
+  { title: '博客文档', className: 'blog' },
+  { title: '资源分享', className: 'resource' },
+  { title: '杂项记录', className: 'other' },
+])
+
 </script>
 <style lang="scss" scoped>
 @use './index.scss';
