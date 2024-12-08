9fb60b9a62672d547d21be391364e496ed48a27b
weizwz <1124725517@qq.com>
2024-12-09 00:53:43 +0800

    修改暗黑动画更丝滑；图标颜色切换为动态变量

--- docs/.vitepress/theme/components/WDark.ts
+++ docs/.vitepress/theme/components/WDark.ts
@@ -1,5 +1,4 @@
 // .vitepress/theme/Layout/toggleAppearance.ts
-// 参考https://github.com/unocss/unocss/blob/main/docs/.vitepress/theme/UnoCSSLayout.vue
 import { nextTick, provide } from 'vue'
 // 增加dark和light时候的动效
 function enableTransitions() {
@@ -8,7 +7,7 @@ function enableTransitions() {
         window.matchMedia('(prefers-reduced-motion: no-preference)').matches
     )
 }
-export function toggleAppearance(isDark) {
+export function toggleDark(isDark) {
     provide(
         'toggle-appearance',
         async ({ clientX: x, clientY: y }: MouseEvent) => {
@@ -17,31 +16,14 @@ export function toggleAppearance(isDark) {
                 isDark.value = !isDark.value
                 return
             }
-            const clipPath = [
-                `circle(0px at ${x}px ${y}px)`, //圆形的半径为0，位于元素的水平x，垂直y的位置
-                `circle(${Math.hypot(
-                    Math.max(x, innerWidth - x),
-                    Math.max(y, innerHeight - y)
-                )}px at ${x}px ${y}px)` // 平方根 hypot
-            ]
+            document.documentElement.style.setProperty('--darkX', x + 'px');
+            document.documentElement.style.setProperty('--darkY', y + 'px');
             // 原生的视图转换动画 https://developer.mozilla.org/zh-CN/docs/Web/API/Document/startViewTransition
             // pnpm add -D @types/dom-view-transitions 解决 document.startViewTransition 类型错误的问题
             await document.startViewTransition(async () => {
               isDark.value = !isDark.value
               await nextTick()
             }).ready
-
-            document.documentElement.animate(
-                { clipPath: isDark.value ? clipPath.reverse() : clipPath },
-                // { clipPath: clipPath },
-                {
-                    duration: 500,
-                    easing: 'ease-in',
-                    pseudoElement: `::view-transition-${
-                        isDark.value ? 'old' : 'new'
-                    }(root)`
-                }
-            )
         }
     )
 }
--- docs/.vitepress/theme/components/WLayout.vue
+++ docs/.vitepress/theme/components/WLayout.vue
@@ -7,8 +7,8 @@ import WDocFooter from './WDocFooter.vue' //文章footer
 // import Comment from './Comment.vue'
 const { isDark } = useData()
 
-import { toggleAppearance } from './WToggleAppearance'
-toggleAppearance(isDark) //实现切换主题过渡动画
+import { toggleDark } from './WDark'
+toggleDark(isDark) //实现切换主题过渡动画
 </script>
 
 <template>
