ec7016a7fa4f41ae75d181685775bb7213888f97
weizwz <1124725517@qq.com>
2024-12-20 16:43:57 +0800

    fix: 修复busuanzi单个文章统计

--- docs/.vitepress/config/head.ts
+++ docs/.vitepress/config/head.ts
@@ -6,7 +6,7 @@ export const createHead = (base: string) => {
     ['link', { rel: 'icon', type: 'image/png', href: base + 'logo.png' }],
     ['meta', { name: 'msvalidate.01', content: '052AEF55670B1059AC662C78C693AED9' }],
     ['meta', { name: 'google-site-verification', content: 'h5ZDnqWDDdodHi5p5KY3IzNtALfigZTMXUZv1TRcSf0' }],
-    ['meta', { property: 'referrer', content: 'no-referrer-when-downgrade' }],
+    ['meta', { name: 'referrer', content: 'no-referrer-when-downgrade' }],
     ['meta', { property: 'og:type', content: 'website' }],
     ['meta', { property: 'og:locale', content: 'zh' }],
     ['meta', { property: 'og:title', content: '唯知笔记 | 一个知识分享网站' }],
