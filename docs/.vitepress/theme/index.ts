84ebf2c78793b9bbfe95a492aa0d28079c10d263
weizwz <1124725517@qq.com>
2025-03-10 18:38:06 +0800

    feat: 更换看图组件为fancybox

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,6 @@
 import type { Theme } from 'vitepress'
-import { EnhanceAppContext, useData, inBrowser, useRoute } from 'vitepress'
-import { h } from 'vue'
+import { EnhanceAppContext, useData, inBrowser, useRouter } from 'vitepress'
+import { h, onMounted, nextTick, onUnmounted } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
@@ -16,9 +16,9 @@ import WDocTitleMeta from './components/WDocTitleMeta.vue' //文章顶部
 import TwoslashFloatingVue from '@shikijs/vitepress-twoslash/client' // type类型悬浮框
 import '@shikijs/vitepress-twoslash/style.css'
 import busuanzi from 'busuanzi.pure.js' // 卜算子统计插件
-import 'viewerjs/dist/viewer.min.css' // 图片查看器
-import imageViewer from 'vitepress-plugin-image-viewer'
-// import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue'
+import '@fancyapps/ui/dist/fancybox/fancybox.css' // 图片查看器
+import { Fancybox } from '@fancyapps/ui'
+import { bindFancybox } from './components/ImgViewer'
 import { NProgress } from 'nprogress-v2/dist/index.js' // 进度条
 import 'nprogress-v2/dist/index.css'
 // 全局样式
@@ -45,28 +45,30 @@ export default {
     app.component('weiz-loading', WLoading)
     app.use(TwoslashFloatingVue)
     app.use(ElementPlus)
-    // 注册全局组件（可选）
-    // app.component('vImageViewer', vImageViewer)
     if (inBrowser) {
       NProgress.configure({ showSpinner: false })
 
-      router.onBeforeRouteChange = () => {
+      const onBeforeRouteChange = () => {
         NProgress.start() // 开始进度条
         busuanzi.fetch()
       }
-      router.onAfterRouteChange = () => {
+      const onAfterRouteChange = () => {
         NProgress.done() // 停止进度条
+        bindFancybox()
       }
+      router.onBeforeRouteChange = onBeforeRouteChange
+      router.onAfterRouteChange = onAfterRouteChange
     }
   },
   setup() {
-    const route = useRoute()
-    // 启用插件
-    imageViewer(route, '.vp-doc', {
-      navbar: false,
-      zoomRatio: .2,
-      minZoomRatio: .1,
-      toggleOnDblclick: false,
+    const router = useRouter()
+    onMounted(() => {
+      bindFancybox()
+    })
+    onUnmounted(() => {
+      Fancybox.destroy()
+      router.onBeforeRouteChange = undefined
+      router.onAfterRouteChange = undefined
     })
   }
 } satisfies Theme
